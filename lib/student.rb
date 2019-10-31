276bdf69f4846ff4606cf533e8e3a3ff56555a86
Aderianna Williams <aderiannaw@yahoo.com>
2019-10-31 04:03:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ def self.drop_table
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][1]
   end
   end
   
