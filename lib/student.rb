d1cebfcf960b2afed516e1630b0ee3006c6d1ed2
Aderianna Williams <aderiannaw@yahoo.com>
2019-10-31 04:06:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,5 +49,25 @@ def self.drop_table
       student = Student.new(name, grade)
       student.save
     end
+    def self.new_from_db(row)
+      id = row[0]
+      name = row[1]
+      grade = row[2]
+
+      self.new(id, name, grade)
+    end
+
+    def self.find_by_name(name)
+      sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE name = ?
+        LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql,name).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
     
 end
